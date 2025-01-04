001c8d5888816b3263b576e8387a0a0205a437d1
AFniljK <suprimchaudhary52@gmail.com>
2025-01-05 00:11:54 +0545

    used reversi::cli

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,39 +1,16 @@
-use std::{
-    net::{SocketAddr, TcpListener},
-    str::FromStr,
-};
-
 use clap::Parser;
-
-#[derive(Debug, Parser)]
-struct Args {
-    // set server host address
-    #[arg(long, short, default_value = "127.0.0.1")]
-    address: String,
-    // set server host port
-    #[arg(long, short, default_value_t = 3000)]
-    port: u16,
-}
-
-impl Args {
-    fn addr(&self) -> SocketAddr {
-        SocketAddr::from_str(&(self.address.clone() + ":" + &self.port.to_string()))
-            .expect("invalid socket address")
-    }
-}
+use reversi::cli::Args;
+use std::net::TcpListener;
 
 fn main() {
     let args = Args::parse();
     let addr = args.addr();
-    let server =
-        TcpListener::bind(args.addr()).expect(&format!("cannot bind to address: {:?}", addr));
+    let server = TcpListener::bind(args.addr()).expect(&format!("cannot bind to address: {:?}", addr));
     println!("listening on address: {:?}", addr);
 
-    let (_kawasaki_stream, kawasaki_addr) =
-        server.accept().expect(&format!("couldn't connect player"));
+    let (_kawasaki_stream, kawasaki_addr) = server.accept().expect(&format!("couldn't connect player"));
     println!("address: {:?} is connected!", kawasaki_addr);
 
-    let (_yamauchi_stream, yamauchi_addr) =
-        server.accept().expect(&format!("couldn't connect player"));
+    let (_yamauchi_stream, yamauchi_addr) = server.accept().expect(&format!("couldn't connect player"));
     println!("address: {:?} is connected!", yamauchi_addr);
 }
