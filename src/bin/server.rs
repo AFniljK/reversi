70a6a1648edfe89cdf80a26eb2b3a0e5cfb419ef
AFniljK <suprimchaudhary52@gmail.com>
2025-01-04 16:42:17 +0545

    cli parser to get socket address for serving

---
+++ src/bin/server.rs
@@ -0,0 +1,24 @@
+use std::{net::SocketAddr, str::FromStr};
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Args {
+    // set server host address
+    #[arg(long, default_value = "127.0.0.1")]
+    address: String,
+    // set server host port
+    #[arg(long, default_value_t = 3000)]
+    port: u16,
+}
+
+impl Args {
+    fn addr(&self) -> SocketAddr {
+        SocketAddr::from_str(&(self.address.clone() + ":" + &self.port.to_string())).expect("invalid socket address")
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("Address: {:?}", args.addr());
+}
