d90b19a896df35d152e221dc34d5e03d0553318d
AFniljK <onlyaforapple@gmail.com>
2025-01-06 19:59:26 +0545

    unwrap changed expect and format

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,21 +1,32 @@
 use clap::Parser;
 use reversi::cli::Args;
-use tungstenite::{accept, Message};
 use std::net::TcpListener;
+use tungstenite::{accept, Message};
 
 fn main() {
     let args = Args::parse();
-    let server = TcpListener::bind(args.addr()).expect(&format!("cannot bind to address: {:?}", args.addr()));
+    let server = TcpListener::bind(args.addr())
+        .expect(&format!("cannot bind to address: {:?}", args.addr()));
     println!("listening on address: {:?}", args.addr());
 
     let (stream, kawasaki_address) = server.accept().expect(&format!("couldn't connect player"));
-    let mut kawasaki = accept(stream).unwrap();
+    let mut kawasaki = accept(stream).expect("cannot upgrade connection to websocket");
     println!("address: {:?} is connected!", kawasaki_address);
 
     let (stream, yamauchi_address) = server.accept().expect(&format!("couldn't connect player"));
-    let mut yamauchi = accept(stream).unwrap();
+    let mut yamauchi = accept(stream).expect("cannot upgrade connection to websocket");
     println!("address: {:?} is connected!", yamauchi_address);
 
-    kawasaki.send(Message::text(format!("{} is your opponent!", yamauchi_address))).unwrap();
-    yamauchi.send(Message::text(format!("{} is your opponent!", kawasaki_address))).unwrap();
+    kawasaki
+        .send(Message::text(format!(
+            "{} is your opponent!",
+            yamauchi_address
+        )))
+        .unwrap();
+    yamauchi
+        .send(Message::text(format!(
+            "{} is your opponent!",
+            kawasaki_address
+        )))
+        .unwrap();
 }
