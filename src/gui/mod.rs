e19635bef3f68adf0557068cf3149606e2c8a389
AFniljK <onlyaforapple@gmail.com>
2025-01-09 00:03:13 +0545

    getting moves from a 'Player', where player could get moves from the board or anywhere else.w

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,14 +1,21 @@
-use std::net::TcpStream;
+use std::collections::HashMap;
 
 use ggez::glam::Vec2;
 use ggez::graphics::{Color, FillOptions, MeshBuilder};
 use ggez::input::keyboard::KeyInput;
 use ggez::{event, graphics};
 use ggez::{event::EventHandler, GameError, GameResult};
-use tungstenite::stream::MaybeTlsStream;
-use tungstenite::{Message, WebSocket};
 
-use crate::{available_captures, bitboard_position, bitboard_rowcol, piece_positions};
+use crate::{bitboard_position, bitboard_rowcol, piece_positions};
+
+pub enum Move {
+    Position(u64),
+    Board,
+}
+
+pub trait Player {
+    fn play_move(&self, config: &PieceConfig) -> Move;
+}
 
 #[derive(Clone, PartialEq)]
 pub struct PieceConfig {
@@ -17,36 +24,55 @@ pub struct PieceConfig {
     pub blacks_play: bool,
 }
 
+impl PieceConfig {
+    pub fn ally_foe(&self) -> (u64, u64) {
+        if self.blacks_play {
+            return (self.black_pieces, self.white_pieces);
+        }
+        return (self.white_pieces, self.black_pieces);
+    }
+}
+
+#[derive(Clone)]
+pub struct BoardConfig {
+    pub piece_config: PieceConfig,
+    pub mesh: HashMap<u64, Color>,
+}
+
+impl BoardConfig {
+    pub fn new(piece_config: PieceConfig) -> BoardConfig {
+        BoardConfig {
+            piece_config,
+            mesh: HashMap::new(),
+        }
+    }
+}
+
 pub struct Board {
     square_size: f32,
-    desired_fps: u32,
-    our_turn: bool,
-    pub debugging: bool,
-    pub piece_config: PieceConfig,
-    keyboard_handler: Box<dyn Fn(KeyInput, &Board) -> PieceConfig>,
-    capture: Box<dyn FnMut(&PieceConfig, u64) -> PieceConfig>,
-    stream: Option<WebSocket<MaybeTlsStream<TcpStream>>>,
+    pub config: BoardConfig,
+    handle_keypress: Box<dyn Fn(KeyInput, &BoardConfig) -> BoardConfig>,
+    capture: Box<dyn Fn(&PieceConfig, u64) -> PieceConfig>,
+    black: Box<dyn Player>,
+    white: Box<dyn Player>,
 }
 
 impl Board {
     pub fn new(
-        board_size: f32,
-        desired_fps: u32,
-        our_turn: bool,
-        keyboard_handler: Box<dyn Fn(KeyInput, &Board) -> PieceConfig>,
-        capture: Box<dyn FnMut(&PieceConfig, u64) -> PieceConfig>,
+        square_size: f32,
         piece_config: PieceConfig,
-        stream: Option<WebSocket<MaybeTlsStream<TcpStream>>>,
+        handle_keypress: Box<dyn Fn(KeyInput, &BoardConfig) -> BoardConfig>,
+        capture: Box<dyn Fn(&PieceConfig, u64) -> PieceConfig>,
+        black: Box<dyn Player>,
+        white: Box<dyn Player>,
     ) -> Board {
         Board {
-            square_size: board_size / 8.0,
-            desired_fps,
-            our_turn,
-            debugging: false,
-            piece_config,
-            keyboard_handler,
+            square_size,
+            config: BoardConfig::new(piece_config),
+            handle_keypress,
             capture,
-            stream,
+            black,
+            white,
         }
     }
 
@@ -76,10 +102,6 @@ impl Board {
         Ok(graphics::Mesh::from_data(ctx, mesh_builder.build()))
     }
 
-    pub fn is_occupied(&self, position: u64) -> bool {
-        (self.piece_config.black_pieces | self.piece_config.white_pieces) & position != 0
-    }
-
     fn grid(&self, ctx: &mut ggez::Context) -> GameResult<graphics::Mesh> {
         let mesh_builder = &mut MeshBuilder::new();
         for i in 1..8 {
@@ -106,59 +128,31 @@ impl Board {
 
 impl EventHandler<GameError> for Board {
     fn update(&mut self, ctx: &mut ggez::Context) -> Result<(), GameError> {
-        if ctx.time.check_update_time(self.desired_fps) {
-            if let Some(stream) = &mut self.stream {
-                if !self.our_turn {
-                    println!("Waiting For Position");
-                    let position: u64 = stream.read().unwrap().to_text().unwrap().parse().unwrap();
-                    println!("Got Position: {}", position);
-                    self.piece_config = (self.capture)(&self.piece_config, position);
-                }
-                self.our_turn = true;
-            } else {
-                if ctx.mouse.button_pressed(event::MouseButton::Left) {
-                    let position = ctx.mouse.position();
-                    let row = (position.y / self.square_size) as u8;
-                    let column = (position.x / self.square_size) as u8;
-                    let position = bitboard_position(row, column);
-
-                    let (ally, foe) = if self.piece_config.blacks_play {
-                        (
-                            self.piece_config.black_pieces,
-                            self.piece_config.white_pieces,
-                        )
-                    } else {
-                        (
-                            self.piece_config.white_pieces,
-                            self.piece_config.black_pieces,
-                        )
-                    };
-
-                    if self.debugging {
-                        self.piece_config = (self.capture)(&self.piece_config, position);
-                        self.our_turn = false;
-                        return Ok(());
-                    }
-                    let keys = match available_captures(ally, foe) {
-                        Some(captures) => captures,
-                        None => {
-                            self.our_turn = false;
-                            self.piece_config.blacks_play = !self.piece_config.blacks_play;
-                            return Ok(());
-                        }
-                    };
-                    if keys.contains_key(&position) {
-                        let piece_config = (self.capture)(&self.piece_config, position);
-                        if let Some(stream) = &mut self.stream {
-                            stream.send(Message::text(position.to_string())).unwrap();
-                            println!("Sent Position: {}", position);
-                        }
-                        self.piece_config = piece_config;
-                        self.our_turn = false;
-                    }
-                }
-            }
+        let desired_fps: u32 = 60;
+        if ctx.time.check_update_time(desired_fps) {
+            return Ok(());
         }
+
+        let player = if self.config.piece_config.blacks_play {
+            &self.black
+        } else {
+            &self.white
+        };
+
+        let position = match player.play_move(&self.config.piece_config) {
+            Move::Position(position) => position,
+            Move::Board => {
+                if !ctx.mouse.button_pressed(event::MouseButton::Left) {
+                    return Ok(());
+                }
+                let position = ctx.mouse.position();
+                let row = (position.y / self.square_size) as u8;
+                let column = (position.x / self.square_size) as u8;
+                bitboard_position(row, column)
+            }
+        };
+        self.config.piece_config = (self.capture)(&self.config.piece_config, position);
+
         Ok(())
     }
 
@@ -169,52 +163,30 @@ impl EventHandler<GameError> for Board {
         repeat: bool,
     ) -> GameResult {
         if !repeat {
-            let config = (self.keyboard_handler)(input, self);
-            self.piece_config = config;
+            self.config = (self.handle_keypress)(input, &self.config);
         }
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut ggez::Context) -> Result<(), GameError> {
-        let mut canvas = graphics::Canvas::from_frame(ctx, Color::from_rgb(255, 162, 103));
-        canvas.draw(
-            &self.colored_mesh(
-                ctx,
-                self.piece_config.black_pieces,
-                Color::from_rgb(66, 66, 66),
-            )?,
-            graphics::DrawParam::new(),
-        );
-        canvas.draw(
-            &self.colored_mesh(
-                ctx,
-                self.piece_config.white_pieces,
-                Color::from_rgb(224, 224, 224),
-            )?,
-            graphics::DrawParam::new(),
-        );
+        let brown = Color::from_rgb(255, 162, 103);
+        let black = Color::from_rgb(66, 66, 66);
+        let grey = Color::from_rgb(224, 224, 224);
+        let mut canvas = graphics::Canvas::from_frame(ctx, brown);
 
-        let (ally, foe) = if self.piece_config.blacks_play {
-            (
-                self.piece_config.black_pieces,
-                self.piece_config.white_pieces,
-            )
-        } else {
-            (
-                self.piece_config.white_pieces,
-                self.piece_config.black_pieces,
-            )
-        };
-        if let Some(captures) = available_captures(ally, foe) {
-            let mut mesh = 0;
-            for key in captures.keys() {
-                mesh |= key;
-            }
-            canvas.draw(
-                &self.colored_mesh(ctx, mesh, Color::MAGENTA)?,
-                graphics::DrawParam::new(),
-            )
+        canvas.draw(
+            &self.colored_mesh(ctx, self.config.piece_config.black_pieces, black)?,
+            graphics::DrawParam::new(),
+        );
+        canvas.draw(
+            &self.colored_mesh(ctx, self.config.piece_config.white_pieces, grey)?,
+            graphics::DrawParam::new(),
+        );
+        for (mesh, color) in self.config.mesh.clone().into_iter() {
+            let drawable = self.colored_mesh(ctx, mesh, color)?;
+            canvas.draw(&drawable, graphics::DrawParam::new());
         }
+
         canvas.draw(&self.grid(ctx)?, graphics::DrawParam::new());
         canvas.finish(ctx)?;
         Ok(())
